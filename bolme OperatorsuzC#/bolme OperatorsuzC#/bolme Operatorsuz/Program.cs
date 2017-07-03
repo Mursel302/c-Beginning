ad95647ef508ac183f81532dfda6a574632a735a
Murselov Mirmursel <mmirmursal.p302@code.edu.az>
2017-07-03 09:47:47 +0400

    C# da ilk proqramim

---
+++ bolme OperatorsuzC#/bolme OperatorsuzC#/bolme Operatorsuz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bolme_Operatorsuz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("bolunen reqemi daxil edin: ");
+            int bolunen;
+            bolunen = Int32.Parse(Console.ReadLine());
+
+            Console.Write("bolen reqemi daxil edin: ");
+            int bolen;
+            bolen = Int32.Parse(Console.ReadLine());
+
+            int netice=0;
+            int qaliq=bolunen;
+
+            while(qaliq>bolen)
+            {
+                qaliq = qaliq - bolen;
+                netice++;
+            }
+
+            Console.WriteLine("{0} reqemini {1} reqemine boldukde netice {2} qaliq ise {3}-dir",bolunen,bolen,netice,qaliq);
+        }
+    }
+}
